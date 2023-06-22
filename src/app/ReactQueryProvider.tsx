1afda70f25af28b9820e40e86d7b698504ee191d
leeharyung <lhr4884@naver.com>
2023-06-22 18:39:24 +0900

    style: apply prettier

--- src/app/ReactQueryProvider.tsx
+++ src/app/ReactQueryProvider.tsx
@@ -1,8 +1,9 @@
 'use client';
 
+import { PropsWithChildren, useState } from 'react';
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { PropsWithChildren, useState } from 'react';
 
 export default function ReactQueryProvider({ children }: PropsWithChildren) {
   const [client] = useState(
