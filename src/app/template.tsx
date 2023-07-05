1566efa59dbd1ebef71753f02eebf9ee9daab5e2
Baik Gwangin <gwangin1999@naver.com>
2023-07-05 13:34:32 +0900

    test: Youtube Player를 대상으로 임시 Portal 적용

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -3,6 +3,8 @@
 import dynamic from 'next/dynamic';
 import { useEffect, useState } from 'react';
 
+import AppPortal from '@/components/app-portal';
+
 const YoutubePlayerController = dynamic(
   () => import('@/components/youtube-player'),
   {
@@ -12,11 +14,15 @@ const YoutubePlayerController = dynamic(
 
 export default function Template({ children }: { children: React.ReactNode }) {
   const [isRendered, setIsRendered] = useState(false);
+
   useEffect(() => setIsRendered(true), []);
+
   return (
-    <div>
-      {isRendered ? <YoutubePlayerController /> : null}
-      {children}
-    </div>
+    <>
+      <AppPortal.Provider portalName="player-portal">
+          {isRendered ? <YoutubePlayerController /> : null}
+          {children}
+      </AppPortal.Provider>
+    </>
   );
 }
