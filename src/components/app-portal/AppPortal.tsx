919eb2ea8a01cad3b783a7c8f87fe423c5ba5200
Baik Gwangin <gwangin1999@naver.com>
2023-07-05 13:34:20 +0900

    fix: 클라이언트 컴포넌트 정의 및 잘못된 구조 수정

--- src/components/app-portal/AppPortal.tsx
+++ src/components/app-portal/AppPortal.tsx
@@ -1,11 +1,13 @@
-import { createContext, useContext, useRef } from 'react';
+'use client'
+
+import { createContext, useContext, useState } from 'react';
 import type { PropsWithChildren, ReactNode } from 'react';
 
 import { createPortal } from 'react-dom';
 
 interface PortalProviderProps {
   children: ReactNode;
-  portalName: string | undefined;
+  portalName?: string | undefined;
 }
 
 const PortalContext = createContext<HTMLDivElement | null>(null);
@@ -14,25 +16,26 @@ const PortalProvider = ({
   children,
   portalName = 'app-portal',
 }: PortalProviderProps) => {
-  const portalRef = useRef<HTMLDivElement | null>(null);
+  const [portalContainer, setPortalContainer] = useState<HTMLDivElement | null>(
+    null,
+  );
 
   return (
-    <PortalContext.Provider value={portalRef.current}>
+    <PortalContext.Provider value={portalContainer}>
       <div
         id={portalName}
         ref={(element) => {
-          if (element && !portalRef.current) portalRef.current = element; // NOTE : PortalRef가 미설정되었으며 DOM 요소가 잡힐 경우 등록
+          if (element && !portalContainer) setPortalContainer(element);
         }}
-      >
-        {children}
-      </div>
+      />
+      {children}
     </PortalContext.Provider>
   );
 };
 
 const PortalWrapper = ({ children }: PropsWithChildren) => {
-  const portalRef = useContext(PortalContext);
-  return portalRef ? createPortal(children, portalRef) : null;
+  const portalContainer = useContext(PortalContext);
+  return portalContainer ? createPortal(children, portalContainer) : null;
 };
 
 const AppPortal = {
