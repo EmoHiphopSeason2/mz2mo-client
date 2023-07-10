3ea7e1f49fe15435e0139116fdbf1ad5befe3142
Baik Gwangin <gwangin1999@naver.com>
2023-07-10 12:30:10 +0900

    feat: 음악 재생 페이지 Page.tsx 1차 간이 작업

--- src/app/playing/page.tsx
+++ src/app/playing/page.tsx
@@ -1,24 +1,45 @@
 'use client';
 
+import { PropsWithChildren } from 'react';
+
 import Header from '@/components/header/Header';
 import MenuButton from '@/components/header/MenuButton';
 import PreviousButton from '@/components/header/PreviousButton';
 
-const PlayingPage = () => {
+interface PlayingPageProps {
+  songTitle: string;
+  singerName: string;
+}
+
+const PlayingPage = ({
+  songTitle = 'Hype Boy',
+  singerName = 'NewJeans',
+}: PropsWithChildren<PlayingPageProps>) => {
   return (
     <>
-      <Header
-        headerLeft={<PreviousButton onClick={() => {}} className="mb-auto" />}
-        headerCenter={
-          <div className="bg-black flex-col w-100 text-center">
-            <h5 className="text-white text-h3">Hype Boy</h5>
-            <p className="text-white text-body2">NewJeans</p>
-          </div>
-        }
-        headerRight={<MenuButton onClick={() => {}} className="mb-auto" />}
-      />
+      <PlayingPage.Header songTitle={songTitle} singerName={singerName} />
     </>
   );
 };
+
+type PlayingPageHeaderProps = Pick<PlayingPageProps, 'songTitle' | 'singerName'>
+
+const PlayingPageHeader = ({
+  songTitle,
+  singerName,
+}: PropsWithChildren<PlayingPageHeaderProps>) => (
+  <Header
+    headerLeft={<PreviousButton onClick={() => {}} className="mb-auto" />}
+    headerCenter={
+      <div className="flex-col w-100 text-center">
+        <h5 className="text-white text-h2">{songTitle}</h5>
+        <p className="text-white text-body3">{singerName}</p>
+      </div>
+    }
+    headerRight={<MenuButton onClick={() => {}} className="mb-auto" />}
+  />
+);
+
+PlayingPage.Header = PlayingPageHeader;
 
 export default PlayingPage;
