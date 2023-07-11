ee6143f230f358a66cfa3690d69a28730102cc72
Baik Gwangin <gwangin1999@naver.com>
2023-07-12 00:16:34 +0900

    feat: 프로그래시브 바 디자인 및 기능 구현

--- src/components/bottom-player/BottomMusicPlayer.tsx
+++ src/components/bottom-player/BottomMusicPlayer.tsx
@@ -1,6 +1,7 @@
-import { memo, useCallback } from 'react';
+import { useCallback } from 'react';
 
-import { useAtom } from 'jotai';
+import clsx from 'clsx';
+import { useAtom, useAtomValue } from 'jotai';
 
 import ControlNextIcon from '@/assets/icons/controlNext.svg';
 import ControlPauseIcon from '@/assets/icons/controlPause.svg';
@@ -9,13 +10,23 @@ import ControlPrevIcon from '@/assets/icons/controlPrev.svg';
 import PlayListIcon from '@/assets/icons/playlist.svg';
 import YoutubePlayer from '@/components/youtube-player/YoutubePlayer';
 import {
+  controlCurrentDurationAtom,
   controlCurrentPlayingAtom,
   controlPlayingStateAtom,
+  playerInstanceAtom,
 } from '@/stores/youtube-controller';
+
+import * as styles from './BottomMusicPlayer.module.scss';
 
 const BottomMusicPlayer = () => {
   const [isPlaying, setIsPlaying] = useAtom(controlPlayingStateAtom);
   const [currentIndex, setCurrentIndex] = useAtom(controlCurrentPlayingAtom);
+  const [currentDuration, setCurrentDuration] = useAtom(
+    controlCurrentDurationAtom,
+  );
+
+  const playerInstance = useAtomValue(playerInstanceAtom);
+  const maxDuration = playerInstance?.getDuration() ?? currentDuration;
 
   const PlayingIcon = isPlaying ? ControlPauseIcon : ControlPlayIcon;
 
@@ -28,39 +39,56 @@ const BottomMusicPlayer = () => {
     nextSong: () => setCurrentIndex(currentIndex + 1),
   };
 
+  const handleCurrentDuration = (e: React.MouseEvent<HTMLProgressElement>) => {
+    const MAX_LENGTH = 480;
+    const { offsetX } = e.nativeEvent;
+    const nextDuration = maxDuration * (MAX_LENGTH - offsetX) / MAX_LENGTH;
+
+    playerInstance?.seekTo(nextDuration, true);
+    setCurrentDuration(nextDuration);
+  }
+
   return (
     <>
-      <section className="w-[480px] bg-gray-900 mx-auto py-4 px-5 flex gap-[17px] items-center">
-        <div className="flex flex-col mr-auto">
-          <h4 className="text-h4 text-white">Hype Boy</h4>
-          <p className="text-body3 text-white">NewJeans</p>
-        </div>
-        <div className="flex gap-8 items-center">
-          <div className="flex gap-[23px] items-center">
-            <ControlPrevIcon
-              width={20}
-              height={20}
-              className="text-white my-auto"
-              onClick={selectPlaylist.prevSong}
-            />
-            <PlayingIcon
+      <section className="flex flex-col w-[480px] mx-auto">
+        <progress
+          value={maxDuration - currentDuration}
+          max={maxDuration}
+          className={clsx(styles.progress, 'w-[480px] h-[3px] rotate-180')}
+          onClick={handleCurrentDuration}
+        />
+        <div className="bg-gray-900 py-4 px-5 flex gap-[17px] items-center">
+          <div className="flex flex-col mr-auto">
+            <h4 className="text-h4 text-white">Hype Boy</h4>
+            <p className="text-body3 text-white">NewJeans</p>
+          </div>
+          <div className="flex gap-8 items-center">
+            <div className="flex gap-[23px] items-center">
+              <ControlPrevIcon
+                width={20}
+                height={20}
+                className="text-white my-auto"
+                onClick={selectPlaylist.prevSong}
+              />
+              <PlayingIcon
+                width={40}
+                height={40}
+                className="text-white cursor-pointer"
+                onClick={togglePlayingState}
+              />
+              <ControlNextIcon
+                width={20}
+                height={20}
+                className="text-white my-auto"
+                onClick={selectPlaylist.nextSong}
+              />
+            </div>
+            <PlayListIcon
               width={40}
               height={40}
               className="text-white cursor-pointer"
-              onClick={togglePlayingState}
-            />
-            <ControlNextIcon
-              width={20}
-              height={20}
-              className="text-white my-auto"
-              onClick={selectPlaylist.nextSong}
             />
           </div>
-          <PlayListIcon
-            width={40}
-            height={40}
-            className="text-white cursor-pointer"
-          />
         </div>
       </section>
       <YoutubePlayer />
