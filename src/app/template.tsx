9624d6ff55540ca058d02f6d878e26316b58b1b4
Gwangin Baik <gwangin1999@naver.com>
2023-07-12 22:06:59 +0900

    [MZO-40] 하단 플레이어 바 UI 구현 (#33)

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,26 +1,21 @@
 'use client';
 
 import dynamic from 'next/dynamic';
-import { useEffect, useState } from 'react';
 
 import AppPortal from '@/components/app-portal';
 
-const YoutubePlayerController = dynamic(
-  () => import('@/components/youtube-player'),
+const YoutubePlayer = dynamic(
+  () => import('@/components/bottom-player'),
   {
     ssr: false,
   },
 );
 
 export default function Template({ children }: { children: React.ReactNode }) {
-  const [isRendered, setIsRendered] = useState(false);
-
-  useEffect(() => setIsRendered(true), []);
-
   return (
     <>
       <AppPortal.Provider portalName="player-portal">
-        {isRendered ? <YoutubePlayerController /> : null}
+        <YoutubePlayer />
         <AppPortal.Provider portalName="modal-portal">
           <AppPortal.Provider portalName="emoji-picker-portal">
             <AppPortal.Wrapper portalName="modal-portal">
