fd9bbf577fc6ae298c94e8d5b185f3de458efdb7
leeharyung <lhr4884@naver.com>
2023-07-13 03:34:56 +0900

    fix: add toaster

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -4,6 +4,7 @@ import dynamic from 'next/dynamic';
 import { useEffect, useState } from 'react';
 
 import AppPortal from '@/components/app-portal';
+import Toaster from '@/components/toast/Toaster';
 
 const YoutubePlayerController = dynamic(
   () => import('@/components/youtube-player'),
@@ -22,13 +23,16 @@ export default function Template({ children }: { children: React.ReactNode }) {
       <AppPortal.Provider portalName="player-portal">
         {isRendered ? <YoutubePlayerController /> : null}
         <AppPortal.Provider portalName="modal-portal">
-          <AppPortal.Provider portalName="emoji-picker-portal">
-            <AppPortal.Wrapper portalName="modal-portal">
-              <div className="z-10">
-                <p>test another portal</p>
-              </div>
-            </AppPortal.Wrapper>
-            {children}
+          <AppPortal.Provider portalName="toast-portal">
+            <AppPortal.Provider portalName="emoji-picker-portal">
+              <AppPortal.Wrapper portalName="modal-portal">
+                <div className="z-10">
+                  <p>test another portal</p>
+                </div>
+              </AppPortal.Wrapper>
+              <Toaster />
+              {children}
+            </AppPortal.Provider>
           </AppPortal.Provider>
         </AppPortal.Provider>
       </AppPortal.Provider>
