a1d2d98f7030ea0e08226d4dce589a1fb823a7c4
Baik Gwangin <gwangin1999@naver.com>
2023-07-12 17:50:30 +0900

    feat: 텍스트가 길 경우 줄임표를 삽입하는 util 추가

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,9 +1,21 @@
 class FormatUtil {
-    static formatTimeToMMSS(timeSecond: number) {
-        const formattedMinute = String(Math.floor(timeSecond / 60)).padStart(2, '0');
-        const formattedSecond = String(Math.floor(timeSecond % 60)).padStart(2, '0');
-        return `${formattedMinute}:${formattedSecond}`
-    }
+  static formatTimeToMMSS(timeSecond: number) {
+    const formattedMinute = String(Math.floor(timeSecond / 60)).padStart(
+      2,
+      '0',
+    );
+    const formattedSecond = String(Math.floor(timeSecond % 60)).padStart(
+      2,
+      '0',
+    );
+    return `${formattedMinute}:${formattedSecond}`;
+  }
+
+  static formatTextEllipsis(text: string, maxLength: number) {
+    return text
+      .slice(0, maxLength + 1)
+      .concat(text.length > maxLength ? '...' : '');
+  }
 }
 
-export default FormatUtil
+export default FormatUtil;
