972f3f1a53294fe3160397f629aa9c0fb2565088
RookieAND <gwangin1999@naver.com>
2023-07-14 02:16:40 +0900

    fix: 병합 과정에서 벌어졌던 문제 재수정

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -3,15 +3,10 @@
 import AppPortal from '@/components/app-portal';
 import ToastProvider from '@/components/toast/ToastProvider';
 
-const YoutubePlayer = dynamic(() => import('@/components/bottom-player'), {
-  ssr: false,
-});
-
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <>
       <AppPortal.Provider portalName="player-portal">
-        <YoutubePlayer />
           <AppPortal.Provider portalName="toast-portal">
             <ToastProvider />
             <AppPortal.Provider portalName="emoji-picker-portal">
