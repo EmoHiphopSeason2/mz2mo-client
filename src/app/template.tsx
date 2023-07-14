30d25bd63c728f2fd67e8b70007f305877695772
leeharyung <lhr4884@naver.com>
2023-07-14 17:54:17 +0900

    fix: fix root template

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -6,18 +6,13 @@ import ToastProvider from '@/components/toast/ToastProvider';
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <>
+      {children}
       <AppPortal.Provider portalName="player-portal">
+        <AppPortal.Provider portalName="emoji-picker-portal">
           <AppPortal.Provider portalName="toast-portal">
             <ToastProvider />
-            <AppPortal.Provider portalName="emoji-picker-portal">
-              <AppPortal.Wrapper portalName="modal-portal">
-                <div className="z-10">
-                  <p>test another portal</p>
-                </div>
-              </AppPortal.Wrapper>
-              {children}
-            </AppPortal.Provider>
           </AppPortal.Provider>
+        </AppPortal.Provider>
       </AppPortal.Provider>
     </>
   );
