120d460060ea339cf903eb52368a5456cea93f74
leeharyung <lhr4884@naver.com>
2023-08-24 21:29:25 +0900

    feat: 임시 이모지 data 추가

---
+++ src/emoji.ts
@@ -0,0 +1,152 @@
+export const TEMP_EMOJI = [
+  {
+    id: '1',
+    unicode: 'U+1F601',
+    icon: '😁',
+  },
+  {
+    id: '2',
+    unicode: 'U+1F61C',
+    icon: '😜',
+  },
+  {
+    id: '3',
+    unicode: 'U+1F621',
+    icon: '😡',
+  },
+  {
+    id: '4',
+    unicode: 'U+1F62D',
+    icon: '😭',
+  },
+  {
+    id: '5',
+    unicode: 'U+1F631',
+    icon: '😱',
+  },
+  {
+    id: '6',
+    unicode: 'U+1F64F',
+    icon: '🙏',
+  },
+  {
+    id: '7',
+    unicode: 'U+2728',
+    icon: '✨',
+  },
+  {
+    id: '8',
+    unicode: 'U+2615',
+    icon: '☕',
+  },
+  {
+    id: '9',
+    unicode: 'U+26C4',
+    icon: '⛄',
+  },
+  {
+    id: '10',
+    unicode: 'U+1F308',
+    icon: '🌈',
+  },
+  {
+    id: '11',
+    unicode: 'U+1F30A',
+    icon: '🌊',
+  },
+  {
+    id: '12',
+    unicode: 'U+1F340',
+    icon: '🍀',
+  },
+  {
+    id: '13',
+    unicode: 'U+1F36D',
+    icon: '🍭',
+  },
+  {
+    id: '14',
+    unicode: 'U+1F383',
+    icon: '🎃',
+  },
+  {
+    id: '15',
+    unicode: 'U+1F389',
+    icon: '🎉',
+  },
+  {
+    id: '16',
+    unicode: 'U+1F393',
+    icon: '🎓',
+  },
+  {
+    id: '17',
+    unicode: 'U+1F3C3',
+    icon: '🏃',
+  },
+  {
+    id: '18',
+    unicode: 'U+1F48A',
+    icon: '💊',
+  },
+  {
+    id: '19',
+    unicode: 'U+1F48D',
+    icon: '💍',
+  },
+  {
+    id: '20',
+    unicode: 'U+1F495',
+    icon: '💕',
+  },
+  {
+    id: '21',
+    unicode: 'U+1F4B0',
+    icon: '💰',
+  },
+  {
+    id: '22',
+    unicode: 'U+1F4AA',
+    icon: '💪',
+  },
+  {
+    id: '23',
+    unicode: 'U+1F525',
+    icon: '🔥',
+  },
+  {
+    id: '24',
+    unicode: 'U+1F60E',
+    icon: '😎',
+  },
+  {
+    id: '25',
+    unicode: 'U+1F608',
+    icon: '😈',
+  },
+  {
+    id: '26',
+    unicode: 'U+1F634',
+    icon: '😴',
+  },
+  {
+    id: '27',
+    unicode: 'U+1F6C1',
+    icon: '🛁',
+  },
+  {
+    id: '28',
+    unicode: 'U+1F40C',
+    icon: '🐌',
+  },
+  {
+    id: '29',
+    unicode: 'U+1F3A4',
+    icon: '🎤',
+  },
+  {
+    id: '30',
+    unicode: 'U+1F4A6',
+    icon: '💦',
+  },
+];
