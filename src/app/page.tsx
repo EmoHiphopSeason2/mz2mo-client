091431bb58be04a3967be86f18933aeec77b31c8
leeharyung <lhr4884@naver.com>
2023-09-19 17:19:38 +0900

    feat: 투표수 많은 노래 ui 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,90 @@
+'use client';
+
 import Image from 'next/image';
 
+import clsx from 'clsx';
+
+import { Header } from '@/components/header';
+import { Playlist, PlaylistButton, usePlaylist } from '@/components/playlist';
+
+const MOST_VOTED_SONGS = [
+  {
+    id: 'MOST_VOTED_SONGS_1',
+    url: 'https://via.placeholder.com/140',
+    title: 'Hype Boys',
+    artist: 'NewJeans',
+  },
+  {
+    id: 'MOST_VOTED_SONGS_2',
+    url: 'https://via.placeholder.com/70',
+    title: '예뻤어 (여름날 우리 X 김민석 (멜로망스))',
+    artist: '김민석 (멜로망스) 예뻤어 (여름날 우리 X 김민석 (멜로망스))2023',
+  },
+  {
+    id: 'MOST_VOTED_SONGS_3',
+    url: 'https://via.placeholder.com/70',
+    title: 'Super Shy',
+    artist: 'NewJeans',
+  },
+];
+
 export default function Home() {
+  const { isOpenPlaylist } = usePlaylist();
+  const PlaylistComponent = isOpenPlaylist ? <Playlist /> : null;
+
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      <div className="z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex">
-        <p className="fixed top-0 left-0 flex justify-center w-full pt-8 pb-6 border-b border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex items-end justify-center w-full h-48 bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="flex gap-2 p-8 pointer-events-none place-items-center lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="h-full bg-black">
+      <Header
+        headerLeft="logo"
+        headerRight={<PlaylistButton className="mb-auto" />}
+      />
+      <div className="p-4">
+        <section className="flex flex-col gap-4 mb-10">
+          <h2 className="text-subtitle1">투표수 많은 노래</h2>
+          <ol>
+            {MOST_VOTED_SONGS.map((item, index) => (
+              <li
+                className={clsx(
+                  'flex gap-4 bg-gray-900 border border-gray-800 rounded-lg g-4 p-4 relative',
+                  index === 0 ? 'mt-0' : 'mt-2',
+                )}
+                key={item.id}
+              >
+                <Image
+                  className={clsx(
+                    'rounded-[4px]',
+                    index === 0 ? 'h-[140px] w-[140px]' : 'h-[70px] w-[70px]',
+                  )}
+                  src={item.url}
+                  alt={item.title}
+                  width={index === 0 ? '140' : '70'}
+                  height={index === 0 ? '140' : '70'}
+                />
+                <div className="flex flex-col overflow-hidden whitespace-nowrap">
+                  <span className="overflow-hidden text-ellipsis text-body3">
+                    {item.title}
+                  </span>
+                  <span className="overflow-hidden font-normal text-ellipsis text-caption">
+                    {item.artist}
+                  </span>
+                  <ol className="flex gap-2 mt-2">
+                    <li>👍</li>
+                    <li>❤️</li>
+                    <li>✨</li>
+                  </ol>
+                </div>
+                <a className="absolute inline-block px-3 py-1 font-normal text-gray-300 border border-gray-300 rounded-full bottom-4 right-4 text-caption">
+                  투표하기
+                </a>
+              </li>
+            ))}
+          </ol>
+        </section>
+        <section>
+          <h2 className="text-subtitle1">내가 들었던 노래</h2>
+        </section>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="grid mb-32 text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="px-5 py-4 transition-colors border border-transparent rounded-lg group hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      {PlaylistComponent}
+    </div>
   );
 }
